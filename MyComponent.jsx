84cb8710673fa922ec8ee0209151a2747e6d4b37
anud12 <supstanta_andu@yahoo.com>
2023-05-21 17:43:45 +0000

    commit

--- MyComponent.jsx
+++
@@ -1,6 +0,0 @@
-const React = require("react");
-module.exports = (props) => (
-    <div>
-        Child {props.name} demo
-        <button onClick={() => console.log("click me")}>Click me</button>
-    </div>)
--- index copy.tsx
+++ index copy.tsx
@@ -1,8 +1,11 @@
-const React = require("react");
-const MyComponent = require("./MyComponent");
+import React from "react";
+import MyComponent from "./MyComponent";
 
 module.exports = (
     <html>
+        <head>
+
+        </head>
         <body>
             <script src="./inline.js" />
             <div className="Hello world"></div>
